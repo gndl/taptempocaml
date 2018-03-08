3f39b5d86d94ffb484d063eb1267ef4e4318f339
Gaëtan Dubreil <gaetan.dubreil@free.fr>
2018-03-08 23:34:31 +0100

    Fix tempo and help message

--- src/i18n.ml
+++ src/i18n.ml
@@ -1,4 +1,15 @@
 
+module Gettext = Gettext.Program
+(
+  struct
+    let textdomain   = "taptempo"
+    let codeset      = None
+    let dir          = None
+    let dependencies = Gettext.init
+  end
+)
+(GettextCamomile.Map)
+
 let gettext line = line
 
 let gettext lines = List.map gettext lines |> String.concat " "
@@ -6,5 +17,3 @@ let gettext lines = List.map gettext lines |> String.concat " "
 let print words =
   gettext words |> print_string;
   flush stdout
-
-
--- src/options.ml
+++ src/options.ml
@@ -1,31 +1,28 @@
+let version = "Taptempocaml 1.0"
 
 let createFromArgs args =
   let sampleSize = ref 5 in
   let resetTime = ref 5 in
   let precision = ref 0 in
   let maxPrecision = 5 in
+  let printVersionAndExit = ref false in
 
-  let usage_msg = I18n.gettext["Usage: taptempo [options]\n";
-                               "\n";
-                               "Options:\n";
-                               "  -h, --help\t\tdisplay this help message\n";
-                               "  -p, --precision\tset the decimal precision of the tempo display\n\t\t\tdefault is";
-                               Printf.sprintf "%d" !precision;
-                               "digits, max is";
-                               Printf.sprintf "%d" maxPrecision;
-                               "digits\n";
-                               "  -r, --reset-time\tset the time in second to reset the computation\n\t\t\tdefault is";
-                               Printf.sprintf "%d" !resetTime;
-                               "seconds\n";
-                               "  -s, --sample-size\tset the number of samples needed to compute the tempo\n\t\t\tdefault is";
-                               Printf.sprintf "%d" !sampleSize;
-                               "samples\n";
-                               "  -v, --version\t\tdisplay the version\n"]
-  in
-  let speclist = [("-s", Arg.Int(fun s -> sampleSize := s), "Sets maximum sample size");
-                  ("-r", Arg.Int(fun r -> resetTime := r), "Sets reset time");
-                  ("-p", Arg.Int(fun p -> precision := p), "Sets bpm precision")]
-  in
+  let usage_msg = I18n.gettext["Usage: taptempo [options]\n"; "Options:\n"] in
+
+  let sampleSizeDoc = I18n.gettext["\tset the number of samples needed to compute the tempo\n\t\t\tdefault is"; string_of_int !sampleSize; "samples"] in
+  let precisionDoc = I18n.gettext["\tset the decimal precision of the tempo display\n\t\t\tdefault is"; string_of_int !precision; "digits, max is"; string_of_int maxPrecision; "digits"] in
+  let resetTimeDoc = I18n.gettext["\tset the time in second to reset the computation\n\t\t\tdefault is"; string_of_int !resetTime; "seconds"] in
+  let versionDoc = I18n.gettext["\t\tdisplay the version"] in
+
+  let speclist = [("-s", Arg.Int(fun s -> sampleSize := s), sampleSizeDoc);
+                  ("-p", Arg.Int(fun p -> precision := p), precisionDoc);
+                  ("-r", Arg.Int(fun r -> resetTime := r), resetTimeDoc);
+                  ("-v", Arg.Unit(fun() -> printVersionAndExit := true), versionDoc);
+                 ] in
   Arg.parse speclist print_endline usage_msg;
 
+  if !printVersionAndExit then (
+    print_endline("\n" ^ version);
+    exit 0
+  );
   (!sampleSize, !resetTime, !precision)
